451da749f3ab23bc53ede491a35d4979415197ff
Raghad Abusamor <raghadmohammad253@gmail.com>
2024-04-30 02:05:21 +0300

    Add .gitignore

--- obj/Release/net8.0/AirportTicketBookingSystem.AssemblyInfo.cs
+++ obj/Release/net8.0/AirportTicketBookingSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirportTicketBookingSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60656b3ec19ede10fe17924001022703e77dd6e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirportTicketBookingSystem")]
